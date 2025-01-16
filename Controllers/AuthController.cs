b0fc09030bf4ea1064b03ba3f04a6a3c0539de74
masum1438 <masum1438@gmail.com>
2025-01-16 10:31:18 +0600

    Login and Reset Password

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,71 @@
+﻿using LoginandReset.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginandReset.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Login()
+        {
+            Session["UserName"] = "";
+            ViewBag.Message = "";
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DoLogin(string txtUserName, string txtPassword)
+        {
+            Session["UserName"] = "";
+            string Message = "Unauthorized";
+
+            BaseAccount baseAccount = new BaseAccount();
+            if (baseAccount.VerifyUser(txtUserName, txtPassword))
+            {
+                Message = "Authorized";
+                Session["UserName"] = txtUserName;
+                //return Redirect("www.google.com"); 
+                return RedirectToAction("About", "Home");
+            }
+            ViewBag.Message = Message;
+            //return RedirectToAction("Dashboard", "Inventory");
+            return View("Login");
+        }
+        [HttpGet]
+        public ActionResult Logout()
+        {
+            Session.Remove("UserName");
+            return View("Login");
+        }
+        [HttpGet]
+        public ActionResult ResetPassword()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DoReset(string txtUserName, string newPassword, string confirmPassword)
+        {
+            Session["UserName"] = "";
+            string Message = "Unauthorized";
+            if (newPassword != confirmPassword)
+            {
+                ViewBag.Message = "Passwords do not match!";
+                return View("ResetPassword");
+            }
+            ResetAccount resetAccount= new ResetAccount();
+            if (resetAccount.VerifyUser(txtUserName, newPassword))
+            {
+                Message = "Authorized";
+                Session["UserName"] = txtUserName;
+                //return Redirect("www.google.com"); 
+                return RedirectToAction("About", "Home");
+            }
+            ViewBag.Message = Message;
+            //return RedirectToAction("Dashboard", "Inventory");
+            return View("Login");
+        }
+    }
+}
---
+++ Models/ResetAccount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace LoginandReset.Models
+{
+    public class ResetAccount
+    {
+        public bool VerifyUser(string txtUserName, string newPassword)
+        {
+            bool status = false;
+            string connString = ConfigurationManager.ConnectionStrings["ConnString"].ToString();
+            SqlConnection sqlConnection = new SqlConnection(connString);
+
+            try
+            {
+                sqlConnection.Open();
+                string query = "UPDATE OSTMember SET Password = @NewPassword WHERE name = @UserName";
+                SqlCommand cmd = new SqlCommand(query, sqlConnection);
+                cmd.CommandType = CommandType.Text;
+
+                // Add parameters to avoid SQL injection
+                cmd.Parameters.AddWithValue("@NewPassword", newPassword);
+                cmd.Parameters.AddWithValue("@UserName", txtUserName);
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+                if (rowsAffected > 0)
+                {
+                    status = true; // Password successfully updated
+                }
+
+                cmd.Dispose();
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                sqlConnection.Close();
+            }
+
+            return status;
+        }
+    }
+}
